722aeb6129ea0ecc6955727a1f2a1c5a1a7d92c4
imdaeshawn <imdaeshawn@gmail.com>
2018-07-29 20:40:49 -0400

    updated index pages and css

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Learn to design and code React apps</h1>
+    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+    
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
