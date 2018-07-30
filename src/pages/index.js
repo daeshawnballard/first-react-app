5726c38f120be3dd729c04493b5e3d7c5862eb25
imdaeshawn <imdaeshawn@gmail.com>
2018-07-29 20:48:27 -0400

    updates css, titles,p, and removed inline css.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
+    
     <h1>Learn to design and code React apps</h1>
     <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
     
