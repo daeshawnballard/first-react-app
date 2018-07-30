f500a3ecef320678dfd8918731d0803444cc4769
imdaeshawn <imdaeshawn@gmail.com>
2018-07-30 18:42:55 -0400

    added a lot more animations, header changes, card components & props.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Card from '../components/Card';
 
 const IndexPage = () => (
   <div>
@@ -30,6 +31,31 @@ const IndexPage = () => (
           </path>
         </svg>
       </div> 
+    </div>
+    <div className="Cards">
+      <h2>11 course, more coming</h2>
+       <div className="CardGroup">
+        <Card 
+          title="Design System" 
+          text="10 sections"
+          image={require('../images/wallpaper.jpg')}
+        />
+        <Card 
+          title="React for Designers" 
+          text="12 sections"
+          image={require('../images/wallpaper2.jpg')}
+        />
+        <Card 
+          title="Sounds Design" 
+          text="5 sections"
+          image={require('../images/wallpaper3.jpg')}
+        />
+        <Card 
+          title="ARKit 2" 
+          text="10 sections"
+          image={require('../images/wallpaper4.jpg')}
+        />
+      </div>
     </div>
   </div>
 )
