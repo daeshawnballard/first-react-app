857deb405c710add8bc369016842f2bc4eb74a3e
imdaeshawn <imdaeshawn@gmail.com>
2018-08-01 09:29:53 -0400

    update with contentful, grapql, and footer

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,17 @@
 module.exports = {
   siteMetadata: {
     title: 'Design+Code 3',
+    description: 'Complete courses about the best tools and design systems. Prototype and build apps with React and Swift. 60 hours of video content and resource materials. No coding experience required.', 
+    keywords: 'react course, react for designers, ios development, sketch app, swift app course, arkit 2, after effects, create sketch plugin'
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: '4oprkrxlv3bs',
+        accessToken: '71ec155c36f700894f0f919f4e066196066c568c1c99cbb974ef0d382b329d14'
+      }
+    }
+  ], 
 }
